126099961205f2eaea3cd58a4b5bddbb14dae209
slroberts <32742307+slroberts@users.noreply.github.com>
2019-06-07 07:59:49 -0400

    Updates

---
+++ precache-manifest.a9060cf997a35fb59b1094dd15be6615.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1b93cf03d72167c7e5efc31e01026ca",
+    "url": "/workload-app/index.html"
+  },
+  {
+    "revision": "debf3def8647aceddfc3",
+    "url": "/workload-app/static/css/main.3f65457c.chunk.css"
+  },
+  {
+    "revision": "edc4cd08bae989c78e02",
+    "url": "/workload-app/static/js/2.427aa0ae.chunk.js"
+  },
+  {
+    "revision": "debf3def8647aceddfc3",
+    "url": "/workload-app/static/js/main.a63f862d.chunk.js"
+  },
+  {
+    "revision": "45bda1e85a6807e3d9f3",
+    "url": "/workload-app/static/js/runtime~main.bd0bd3f6.js"
+  }
+]);
